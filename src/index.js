b01197136002b00b9238c39d8ae7ab1346c7a810
Vishwajeet <vishawajit_s@parthinfotech.net>
2018-10-26 14:06:22 +0530

    encryption using iv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Readline from './service/readline'
 import DB from './service/dbConnection'
+
 const crypto = require('crypto')
 
 let dbQuestion = new Readline()
 let Database = new DB()
-const secret = 'abcdefg'
+const secret = 'crypto.randumbytes(32).crypto.ra';
 
 let dbQuestions = [
   'Host :- ',
@@ -18,7 +19,7 @@ let dbQuestions = [
 
 let dbConfig = []
 let Tables = []
-async function ask(questions) {
+async function ask (questions) {
   for (const question of questions) {
     let ans = await dbQuestion.askQuestion(question)
     dbConfig.push(ans)
@@ -27,71 +28,55 @@ async function ask(questions) {
   return dbConfig
 }
 
-function makeConfigObject(dbConfig) {
+function makeConfigObject (dbConfig) {
   let obj = Database.makeConnectionObject(dbConfig)
   return obj
 }
 
-async function makeConnection(conObj) {
+async function makeConnection (conObj) {
   return Database.getConnection(conObj)
 }
-
-// async function getAllTables (connection) {
-//   return new Promise ((resolve, reject) => {
-//     connection.query('SHOW TABLES', (err, rows) => {
-//       if (err) {
-//         reject(err)
-//       }
-//       resolve(rows)
-//     })
-//   })
-// }
-
-// async function getColumnData(connection, inputData) {
-//   return new Promise((resolve, reject) => {
-//     connection.query(`SELECT ${inputData[6]} FROM ${inputData[5]}`, (err, rows) => {
-//       if (err) {
-//         reject(err)
-//       }
-//       resolve(rows)
-//     })
-//   })
-// }
 
 let streamListener = (connection, options) => {
   let promisArray = []
   return new Promise((resolve, reject) => {
-    connection.query(`SELECT ${options[6]} FROM ${options[5]}`)
+    connection.query(`SELECT * FROM ${options[5]}`)
       .on('error', (err) => {
         reject(err)
       })
       .on('result', function (row) {
-        promisArray.push(processRow(row))
+        processRow(row.GscName).then(res => {
+          connection.query(`UPDATE gscnames SET GscName = '${res}' Where GscID = ${row.GscID}`, function (err, result) {
+            if (err) {
+              console.log(err)
+            }
+          })
+        })
       })
       .on('end', () => {
         Promise.all(promisArray)
           .then(res => {
-            resolve(res)
+            console.log('done')
           })
       })
   })
 }
 
-function processRow(row) {
-  const cipher = crypto.createCipher('aes-256-ctr', secret)
-  console.log(row.gscname, '******************')
-  let encrypted = cipher.update(row.gscname, 'utf8', 'hex')
+function processRow (row) {
+  const IV = Buffer.from(crypto.randomBytes(16))
+  const cipher = crypto.createCipheriv('aes-256-ctr', secret, IV)
+  let encrypted = cipher.update(row, 'utf8', 'hex')
   encrypted += cipher.final('hex')
-  return encrypted
+  let encryptedTXT = IV.toString('hex') + '-' + encrypted
+  return Promise.resolve(encryptedTXT)
 }
 
-async function main() {
+async function main () {
   let dbConfig = await ask(dbQuestions)
   let conObj = makeConfigObject(dbConfig)
   let connection = await makeConnection(conObj)
-  // let tables = await getAllTables(connection)
   let test = await streamListener(connection, dbConfig)
-  console.log(test)
+  dbQuestion.close()
 }
 
 main()
